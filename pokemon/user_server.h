7299a6f4c99fcbb26737619a1a07c6fb5d3641c0
LiuSecone <liusicong1998@outlook.com>
2018-12-09 21:10:05 +0800

    add user_server

--- pokemon/user_server.h
+++ pokemon/user_server.h
@@ -3,4 +3,20 @@
 #ifndef USER_SERVER_H
 #define USER_SERVER_H
 
+#include <string>
+#include <vector>
+
+#include "hero.h"
+
+class user_server {
+public:
+    const std::string user_name;
+    const std::string password_hash;
+    std::vector<std::shared_ptr<hero>> heros;
+    explicit user_server(std::string &&name, std::string &&hash) :
+    user_name(name),
+    password_hash(hash) {
+    }
+};
+
 #endif //USER_SERVER_H
